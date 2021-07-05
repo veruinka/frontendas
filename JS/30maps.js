ea199693b7527884903f4b4fdccb9904bb914b0d
veruinka <verkastudentka@gmail.com>
2021-07-05 17:29:59 +0300

    5ijulia

--- JS/30maps.js
+++ JS/30maps.js
@@ -3,3 +3,96 @@ const setas = new Set();
 setas.add('grybas');
 setas.add('aguras');
 setas.add('pupelė (ankštinė)');
+
+
+//1.issortinti seta
+const A = [...setas].sort();
+setas.clear();
+A.forEach(v => setas.add(v));
+
+console.log(setas);
+
+
+//2.setą įrašyti į localstorage, tada nuskaityti į setasCopy kintamąjį
+
+
+//3.surusiuoti, kad setas su didziausiu skaiciu vienetu eitu pirmas
+const hard = [
+    new Set([2,8,7]),
+    new Set([2,0,7,8,7]),
+    new Set([2,8,0,7,1,9]),
+    new Set([2,8,7,0])
+];
+
+
+//4.padaryti kad, masyvai eina pirmi, tada eina setai
+console.log(hard instanceof Array);
+console.log(setas instanceof Set);
+const easy = [
+    [1,8],
+    new Set([7, 9]),
+    new Set(),
+    [1,8,7],
+    new Set([7, 9, 11]),
+    [],
+    new Set([7, 9, 71, 11, 45]),
+];
+
+
+//5.masyve easy palikti tik setus
+
+
+
+/////////////////////////////Map//////////////////////////////////////
+const map1 = new Map();
+map1.set('Petras', 'jo stalciuko turinys');
+map1.set('Jonas', 'jo stalciukas');
+map1.set('Mama', 'jos stalciukas<3');
+
+console.log('paieska:', map1.has('Mama'));
+console.log('stalciuko reveal:', map1.get('Jonas'));
+
+for(const val of map1){
+    console.log('for of:', val);
+};
+
+const mapAr = [...map1]
+console.log('...:', mapAr);
+
+console.log(map1);
+
+
+//1.
+const map = new Map();
+
+map.set('Petras', 'kas guli Petro stalčiuje');
+map.set('Šarikas', 'kas guli Šariko būdoje');
+map.set('Janotas', 'ką šiukšlių dėžėjė rado Janotas');
+
+for(const val of map){
+    console.log('1:', val[0]);
+};
+
+
+//2.mapo įrašyti į localstorage, tada nuskaityti į mapCopy kintamąjį
+
+
+
+//3.sudėlioti tokia tvarka: masyvai, mapai, setai
+const notSoEasy = [
+    [1,8],
+    new Set([7, 9]),
+    new Set(),
+    new Map([['a',2], ['b',2], ['c',2]]),
+    [1,8,7],
+    new Set([7, 9, 11]),
+    [],
+    new Set([7, 9, 71, 11, 45]),
+    new Map([['a',2], ['c',2]]),
+];
+
+
+
+
+
+
---
+++ JS/31sort+filter.js
@@ -0,0 +1,42 @@
+//1.sort
+//2.sort + callback
+//3.sort + callback + desc
+const m1 = ['z', 's', 'a', 'd'];
+
+//4.Padaryti kad 2 rūšiavimas įvyktų paspaudus mygtuką.
+
+//5.Padaryti kad 3 rūšiavimas įvyktų paspaudus mygtuką.
+
+//6.pagal masyvo antrą elementą ASC
+//7.pagal masyvo pirmą elementą DESC
+//8.pagal masyvo antro elemento antrą simbolį ASC
+//9.pagal masyvo pirmo elemento trčią simbolį DESC
+const m2 = [
+    ['zru', 'six'],
+    ['rot', 'dzi'],
+    ['uxm', 'ycw'],
+    ['otr', 'sth']
+];
+
+//10.pagal katę DESC
+//11.pagal šunį ASC
+//12.pagal gyvatės iš šuns sumą DESC
+const m3 = [
+    new Map([['dog', 5], ['cat', 7], ['snake', 58]]),
+    new Map([['snake', 33], ['dog', 14], ['cat', 8]]),
+    new Map([['dog', 51], ['cat', 77], ['snake', 23]]),
+    new Map([['cat', 10], ['dog', 23], ['snake', 17]])
+];
+
+//12 pagal masyvų ilgį
+//13 pagal masyvų elementų sumą, abu ASC
+const m4 = [
+    [2,5,8,9,7,4,5,2,1,5,8,9,3],
+    [3,6,9,8,5,4],
+    [2,5,4,7,9,5,4,1,2,3,6,9,8,4,7],
+    [5,2,1,4,7,8,5,3,1]
+];
+
+//14.su filter ištrinti skaičius didesnius nei 7
+//15.su filter iš m3 masyvo su mapais ištrinti tuos mapus, kuriuose šuo mažiau nei 10
+const m5 = [4,5,6,9,45,4,7,8,5,2,4,8,2,1,3,32,41,85,6,8,5,6,78,11,6,8,];
