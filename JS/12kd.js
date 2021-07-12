b1a99edf2c8d339108b3b140984eaa4558b43f03
veruinka <verkastudentka@gmail.com>
2021-07-12 19:22:24 +0300

    10 ijulia

--- JS/12kd.js
+++ JS/12kd.js
@@ -1,7 +1,6 @@
 // // Parašyti dvi funkcijas be parametrų red() ir green(). 
 // Funkcijos turi grąžinti atitinkamus stringus “red” ir “green”. 
 // Pademostruoti veikimą;
-
 function red() {
     let A = 'red';
     return A;
@@ -18,10 +17,10 @@ let BB = green();
 
 console.log('1:', AA, BB);
 
+
 // Parašyti funkciją color(what). 
 // Funkcija turi grąžinti splvą, priklausomai nuo argumento: jeigu what == 1 - stringą “white”, jeigu what == 2 - stringą “black”,  kitais atvejais - “no color”. 
 // Pademostruoti veikimą;
-
 function color(what) {
     if (what == 1) {
         return 'White';
@@ -36,10 +35,10 @@ function color(what) {
 
 console.log('2:', color(2));
 
+
 // Parašykite dvi funkcijas sum(a, b) ir diff(a, b). 
 // Pirma finkcija turi suskaičiuoti sumą (a+b), o kita skirtumą (a-b) su argumentais a, b ir grąžinti rezultatą. 
 // Pademostruoti veikimą;
-
 function sum(a, b) {
     return a + b;
 };
@@ -53,9 +52,9 @@ let b = 3;
 
 console.log('3:', sum(a, b), diff(a, b));
 
+
 // // Papildyti prieš tai buvusį uždavinį, kad a ir b parametrai turėtų default argumentus lygius 0. 
 // Pademostruoti veikimą;
-
 function sum(a = 0, b = 0) {
     return a + b;
 };
@@ -65,6 +64,7 @@ function diff(a = 0, b = 0) {
 };
 
 console.log('4:', sum(a), diff(a));
+
 
 // // Sukurkite masyvą ab [‘A’, ‘B’], masyvą cd [‘C’, ‘D’] ir masyvą fe [‘F’, ‘E’] . 
 // Masyvai nėra funkcijos viduje. 
@@ -84,12 +84,12 @@ function changeArray(array){
 let mas = changeArray(fe);
 console.log('5:', mas);
 
+
 // Sukurti objektą car1 {make: ’volvo’} ir car2 {make: ’mb’}. 
 // Objektai nėra funkcijos viduje. 
 // Sukurti funkciją changeMake(car, make), kuri keistų perduodamo objekto car savybę į perduodamo make argumento reikšmę. 
 // Pvz iškvietus funkciją su argumentais changeMake(car1,’ZaZ’) car1 objekte savybė pasikeistų į ‘ZaZ’.  
 // Pademostruoti veikimą;
-
 let car1 = {make: 'volvo'};
 let car2 = {make: 'mb'};
 
@@ -101,35 +101,74 @@ changeMake(car1,'ZaZ');
 
 console.log('6:', car1);
 
+
 // Paršyti funkcija kuri masyve visas jo reikšmes pakeistų į atsitiktinai sugeneruotus spalvų stringus: “red”, “blue” arba “green”. 
 // Funkcija turi turėti du argumentus, vienas generuojamas masyvas, o kitas funkcija - splvų generatorius. 
 // Pademostruoti veikimą;
+function rand(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+let masyv = [4, 2, 9];
+let str = ['red', 'blue', 'green'];
 
+function pakeisti (a, b){
+    a[0] = b[rand(0, 2)];
+    a[1] = b[rand(0, 2)];
+    a[2] = b[rand(0, 2)];
+};
 
-console.log('7:', );
+pakeisti(masyv, str);
+
+console.log('7:', masyv);
+
 
 // Yra masyvas [8, 9, 7, 6, 12, 48, 0, 6] 
 // Išrūšiuoti masyvą naudojan sort ir atskirą rūšiavimo funkciją. 
 // Pademostruoti veikimą;
+let ar = [8, 9, 7, 6, 12, 48, 0, 6];
 
+ar.sort(function (a, b) {
+    return a - b;
+});
 
+console.log('8:', ar);
 
-console.log('8:', );
 
 // Yra masyvas [“Pingvinas”, “Zebras”, “Voras”, “Vilkas”, “Zuikis”]. 
 // Sukurti naują masyvą naudojant map ir atskirą funkciją, kuriame atitinkami indeksai atitiktų žodžių ilgį. 
 // Pademostruoti veikimą;
+let arr = ['Pingvinas', 'Zebras', 'Voras', 'Vilkas', 'Zuikis'];
 
+const arr2 = arr.map(function(a) {
+    return a.length;
+});
 
+console.log('9:', arr2);
 
-console.log('9:', );
 
 // Yra masyvas [8, “B”, 9, 7, “A”, 6, 12, “B”, “B”, “A”, 48, 0, “A”, 6] 
 // Išrūšiuoti masyvą nuo didžiausio iki mažiausio naudojant sort ir atskirą rūšiavimo funkciją. 
 // “A” raidės turi eiti pačiam gele, o “B” raidės pačiame priekyje. 
 // Pademostruoti veikimą;
+let ara = [8, 'B', 9, 7, 'A', 6, 12, 'B', 'B', 'A', 48, 0, 'A', 6];
 
+ara.sort(function(a, b) {
+    if (a === 'A'){
+       return 1;
+    }
+    if (b === 'B') {
+        return 1;
+    }
+    if (b === 'A'){
+        return -1;
+     }
+    if (a === 'B') {
+        return -1;
+    }
+    return b - a;
+});
 
-
-console.log('10:', );
+console.log('10:', ara);
--- JS/18kartojimas.js
+++ JS/18kartojimas.js
@@ -3,12 +3,15 @@
 const A = document.querySelector('h1');
 A.style.color = 'darkgreen';
 
+
 // b. Tagui i pridėti klasę small;
 const B = document.querySelector('i');
 B.classList.add('small');
 
+
 // c. Iš tago h1 pašalinti klasę main;
 A.classList.remove('main');
+
 
 // d. Tik tam h2 tagui, kuris iškart eina po h1 tagui pridėti klasę first ir pašalinti klasę main;
 const C = document.querySelector('h1 + h2');
@@ -16,11 +19,13 @@ const C = document.querySelector('h1 + h2');
 C.classList.add('first');
 C.classList.remove('main');
 
+
 // e. Pirmam span tagui, kuris yra h2 viduje sumažinti fonto dydį iki 10px ir nudažyti pilkai
 const D = document.querySelector('h2 span');
 
 D.style.fontSize = '10px';
 D.style.color = 'grey';
+
 
 // 2. Elemetų grupės (nodeList) skaičiavimus išvest į consolę
 // a. Suskaičiuoti kiek yra h2 tagų;
@@ -35,6 +40,7 @@ let c = 0;
 document.querySelectorAll('h2').forEach( () => c++);
 
 console.log('a:', c);
+
 
 // b. Suskaičiuoti kiek yra h2 tagų, kurie neturi klasės first
 // let cou = 0;
@@ -51,6 +57,7 @@ document.querySelectorAll('h2').forEach( (h2) => {
 }});
 console.log('b:', cou);
 
+
 // c. Visus h2 tagus nuspalvinti šviesiai mėlynai
 // for (let index = 0; index < E.length; index++) {
 //     E[index].style.color = 'lightblue';
@@ -58,13 +65,13 @@ console.log('b:', cou);
 
 document.querySelectorAll('h2').forEach ((h2) => h2.style.color = 'lightblue');
 
+
 // d. Tagams kurie yra div su klase prices viduje pridėti klasę price-tag;
 // const F = document.querySelector('div.prices'); 
 // F.classList.add('price-tag');
 
 document.querySelector('.prices').classList.add('price-tag');
 
-// F.querySelector('h2').forEach ((h2) => h2.classList.add('price-tag'));
 
 // e. Pabraukti visus tagus su klase new;
 // const G = document.querySelectorAll('.new');
@@ -74,6 +81,7 @@ document.querySelector('.prices').classList.add('price-tag');
 // }
 
 document.querySelectorAll('.new').forEach ((e) => e.style.textDecoration = 'underline');
+
 
 // f. Suskaičiuoti kiek yra gyvūnų kategorijų (jos yra ul tagai);
 // const H = document.querySelectorAll('ul');
@@ -88,6 +96,7 @@ document.querySelectorAll('ul').forEach (() => count++);
 
 console.log('f:', count);
 
+
 // g. Tagus ul apibraukite rėmeliais ir uždėkite 15px paddingą viršuje ir apačioje ir 50px paddingą kairėje ir dešinėje;
 // for (let index = 0; index < H.length; index++) {
 //     H[index].style.border = '1px solid black';
@@ -98,6 +107,7 @@ document.querySelectorAll('ul').forEach ((e) => {
     e.style.border = '1px solid black'; 
     e.style.padding = '15px 50px';
 });
+
 
 // h. Suskaičiuoti kiek yra naujų gyvūnų (su klase new);
 // const I = document.querySelectorAll('.new');
@@ -111,6 +121,7 @@ co = 0;
 document.querySelectorAll('.new').forEach (() => co++);
 
 console.log('h:', co);
+
 
 // i. Suskaičiuoti atskirai kiek yra naujų gyvūnų kiekvienoje kategorijoje
 // const H = document.querySelectorAll('ul');
@@ -139,6 +150,7 @@ document.querySelectorAll('ul').forEach (ul => {
 
 console.log('i:', cc2);
 
+
 //3. Elementų events
 // a. Padaryti tai ką liepia mygtukai Header sekcijoje;
 // const K = document.querySelector('#h1-color');
@@ -163,6 +175,7 @@ document.querySelector('#h1-font').addEventListener('click', () =>{
     document.querySelector('h1').style.fontSize = "10px";
 });
 
+
 // b. Padaryti, kad paspaudus ant i tago jis pakeistų fonto svorį į bold;
 // const N = function() {
 //     B.style.fontWeight = 'bold';
@@ -171,6 +184,7 @@ document.querySelector('#h1-font').addEventListener('click', () =>{
 // B.addEventListener('click', N);
 
 document.querySelector('i').addEventListener('click', () => document.querySelector('i').style.fontWeight = 'bold');
+
 
 // c. Padaryti, kad paspaudus ant tago su klase prices, backgroundas pasikeistų į pilką, o paspaudus dar kartą vėl grįžtu į baltą spalvą;
 // const M = document.querySelector('.prices');
@@ -186,6 +200,7 @@ document.querySelector('.prices').addEventListener('click', (e) => {
     e.target.classList.toggle('white');
     });
 
+
 // d. Padaryti, kad paspaudus ant tago su id contacts, tam tagui būtų pridėta css savybė color;
 // const S = document.querySelector('#contacts');
 // const T = function() {
@@ -196,6 +211,7 @@ document.querySelector('.prices').addEventListener('click', (e) => {
 
 document.querySelector('#contacts').addEventListener('click', () => document.querySelector('#contacts').style.color = 'purple');
 
+
 // e. Padaryti taip, kad paspaudus ant padidinti, esančio tage su id contacts, tagui su id contacts būtų pridėta css savybė fontSize = 20px;
 // const U = S.querySelector('u');
 // const Q = function() {
@@ -205,6 +221,7 @@ document.querySelector('#contacts').addEventListener('click', () => document.que
 // U.addEventListener('click', Q);
 
 document.querySelector('u').addEventListener('click', e => document.querySelector('#contacts').style.fontSize = '20px');
+
 
 // f. Padaryti taip, kad paspaudus ant X, esančio tage su id contacts, pridėtos tage su id contacts savybės būtų panaikintos https://stackoverflow.com/questions/18691655/remove-style-on-element
 // const V = S.querySelector('b');
@@ -220,6 +237,7 @@ document.querySelector('b').addEventListener('click', (e) => {
     document.querySelector('#contacts').style = null;
 });
 
+
 // g. Padaryti tai ką liepia mygtukai Header2 sekcijoje;
 // const X = document.querySelector('#h1-color-back');
 // const Y = function() {
@@ -228,7 +246,7 @@ document.querySelector('b').addEventListener('click', (e) => {
 
 // X.addEventListener('click', Y);
 
-document.querySelector('#h1-color-back').addEventListener('click', e => document.querySelector('h1').style.color = null);
+document.querySelector('#h1-color-back').addEventListener('click', () => document.querySelector('h1').style.color = "darkgreen");
 
 // const Z = document.querySelector('#h1-font-back');
 // const AA = function() {
@@ -237,7 +255,8 @@ document.querySelector('#h1-color-back').addEventListener('click', e => document
 
 // Z.addEventListener('click', AA);
 
-document.querySelector('#h1-font-back').addEventListener('click', e => document.querySelector('h1').style.fontSize = null);
+document.querySelector('#h1-font-back').addEventListener('click', () => document.querySelector('h1').style.fontSize = null);
+
 
 //4. Elementų grupių events
 //a. Padaryti, kad dukartus paspaudus ant naujų gyvūnų jie nusispalvintu raudonai https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
@@ -255,6 +274,7 @@ document.querySelectorAll('.new').forEach(animal => {
     });
 });
  
+
 //b. Padaryti, kad paspaudus ant gyvūno jis būtų atvaizduojamas 130% didesniu fonto dydžiu. “PATINKA” tas neturi galioti.
 // const BB = document.querySelectorAll('li');
 
@@ -267,11 +287,11 @@ document.querySelectorAll('.new').forEach(animal => {
 //     }
 // }
 
-document.querySelectorAll('li').forEach( e => {
-    if (!e.classList.contains('like-button')){ (e, i) => {
-        e[i].addEventListener('click', e => e.style.fontSize = "130%");
-    };
-}});
+document.querySelectorAll('li').forEach ((e, i) => document.querySelectorAll('li')[i].addEventListener('click', () => {
+    if (!document.querySelectorAll('li')[i].classList.contains('like-button')){
+        document.querySelectorAll('li').forEach (() => document.querySelectorAll('li')[i].style.fontSize = "130%")
+}}));
+
 
 //c. Padaryti, kad paspaudus ant “PATINKA”, atitinkamai sekcijai būtų priskirta klasė like;
 // const DD = document.querySelectorAll('.like-button');
@@ -284,9 +304,13 @@ document.querySelectorAll('li').forEach( e => {
 //     DD[j].addEventListener('click', EE);
 // };
 
-document.querySelectorAll('.like-button').forEach (e => (e.addEventListener('click', () =>{
-    document.querySelectorAll('ul').forEach ((e, i) => (e.classList.toggle('like')));
+const XX = document.querySelectorAll('ul');
+const DD = document.querySelectorAll('.like-button');
+
+DD.forEach ((e, i) => (DD[i].addEventListener('click', () =>{
+    XX.forEach (() => (XX[i].classList.toggle('like')));
 })));
+
 
 //5. Dinaminis elementų kūrimas
 //a. Dinamiškai su JS pridėti naują kainą “Senjorai tik: 1.99 eur”;
@@ -294,7 +318,8 @@ const E = document.createElement('h2');
 const F = document.createTextNode('Senjorai tik: 1.99 eur');
 E.appendChild(F);
 
-const G = document.querySelector('div.prices').appendChild(E);
+document.querySelector('div.prices').appendChild(E);
+
 
 //b. Dinamiškai su JS Pridėti naują kainą “Senjorų grupė iki 10: tik 5.99 eur”
 // Padaryti, kad pridėtas elementas turėtų klasę new ir ant jo paklikinus jis pasidarytų žalias;
@@ -312,10 +337,23 @@ K.classList.add('new');
 K.addEventListener('click', O);
 
 //c. Dinamiškai su JS kiekvienoje gyvūnų kategorijoje po “PATINKA” pridėkite dar vieną li elementą “NEPATINKA”, kurį paspaudus atitinkamoje sekcijoje būtų nuimta klasė like
+const EE = document.createElement('li');
+const FF = document.createTextNode('NEPATINKA');
+EE.appendChild(FF);
+
+const DD1 = document.querySelectorAll('li.like-button');
 
 
 
+const DD2 = document.querySelectorAll('.like-button');
+// const XX = document.querySelectorAll('ul');
 
+// for (let j = 0; j < DD.length; j++) {
+//     const EE = function () {
+//         XX[j].classList.toggle('like');
+//     };
+//     DD[j].addEventListener('click', EE);
+// };
 
 //d. Dinamiškai su JS sukurkite naują mygtukų grupę HEADER 3 naudojant analogišką html tagų struktūrą kaip ir HEADER 1 ir HEADER 2. 
 // Pirmas mygtukas vadintųsi, “Pabraukti H1 tagą”, o antras “Nepabraukti H1 tagą”. 
